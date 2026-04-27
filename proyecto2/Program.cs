9ecea80672bfe36f45124c498e91ffa037010c24
Christopher Tzul <chrisdavidtzul@gmail.com>
2026-04-27 09:04:20 -0600

    login_inicio

--- proyecto2/Program.cs
+++ proyecto2/Program.cs
@@ -1,1 +1,30 @@
-﻿//Inicio del repositorio colaborativo
+﻿//Menu de inicio y declaracion de credenciales
+//password admin123+
+//user player1
+
+//variable de seguro, sirve como verificacion de las credenciales
+bool seguro;
+
+//login de inicio
+do {
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Ingrese su usuario:");
+    Console.ForegroundColor= ConsoleColor.Cyan;
+    string usuario = Console.ReadLine();
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Ingrese su usuario:");
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    string pssw = Console.ReadLine();
+    if(usuario == "player1" && pssw == "admin123+")
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Credenciales correctas");
+        seguro = true;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Credenciales incorrectas, revise nuevamente");
+        seguro= false;
+    }
+}while(seguro ==  false);
