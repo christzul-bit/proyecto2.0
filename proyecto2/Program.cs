4e8ee3b009d327a7c5337f1874db034700d4bf6e
Christopher Tzul <chrisdavidtzul@gmail.com>
2026-04-28 18:34:28 -0600

    Ocultando_password

--- proyecto2/Program.cs
+++ proyecto2/Program.cs
@@ -4,7 +4,27 @@
 
 //variable de seguro, sirve como verificacion de las credenciales
 bool seguro;
-
+string password;
+void IngresarPssw()
+{
+    password = "";
+    Console.WriteLine("Ingrese su contraseña");
+    ConsoleKeyInfo tecla;
+    do
+    {
+        tecla = Console.ReadKey(true);
+        if(tecla.Key != ConsoleKey.Enter && tecla.Key != ConsoleKey.Backspace)
+        {
+            password += tecla.KeyChar;
+            Console.Write("*");
+        }else if (tecla.Key == ConsoleKey.Backspace && password.Length > 0)
+        {
+            password = password.Substring(0, (password.Length - 1));
+            Console.Write("\b \b");
+        }
+    }while (tecla.Key != ConsoleKey.Enter);
+    Console.WriteLine();
+}
 //login de inicio
 do {
     Console.ForegroundColor = ConsoleColor.Blue;
@@ -12,10 +32,10 @@ do {
     Console.ForegroundColor= ConsoleColor.Cyan;
     string usuario = Console.ReadLine();
     Console.ForegroundColor = ConsoleColor.Blue;
-    Console.WriteLine("Ingrese su usuario:");
+    Console.WriteLine("Ingrese su contraseña:");
     Console.ForegroundColor = ConsoleColor.Cyan;
-    string pssw = Console.ReadLine();
-    if(usuario == "player1" && pssw == "admin123+")
+    IngresarPssw();
+    if(usuario == "player1" && password == "admin123+")
     {
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("Credenciales correctas");
