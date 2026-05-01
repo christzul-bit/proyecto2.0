da8927a598280fc6b22d46cce9c314b001271e1c
verno <verno@ASUS>
2026-05-01 15:47:06 -0600

    soldado y torre class

--- pieces/king/Class1.cs
+++ pieces/king/Class1.cs
@@ -16,4 +16,36 @@
             Columna = c;
         }
     }
+
+    public class Tower:Pieza
+    {
+        public string Figura => "|T|";
+        public string Color { get; set; } = "";
+
+        public void MostrarPosicion(string[,] tablero)
+        {
+            tablero[Fila, Columna] = Figura;
+        }
+
+        public void MoverTorre(int f, int c)
+        {
+            Fila = f;
+            Columna = c;
+        }
+    }
+
+    public class Soldier:Pieza
+    {
+        public string Figura => "|S|";
+        public string Color { get; set; } = "";
+        public void MostrarPosicion(string[,] tablero)
+        {
+            tablero[Fila, Columna] = Figura;
+        }
+        public void MoverSoldado(int f, int c)
+        {
+            Fila = f;
+            Columna = c;
+        }
+    }
 }
